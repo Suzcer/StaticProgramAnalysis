77b363c7f5abcf26c4e579b2aace82ad8a7a09a9
201250104 <201250104@smail.nju.edu.cn>
2022-07-24 13:54:03 +0800

    Lab1

---
+++ A1/tai-e/src/main/java/pascal/taie/analysis/dataflow/solver/IterativeSolver.java
@@ -0,0 +1,67 @@
+/*
+ * Tai-e: A Static Analysis Framework for Java
+ *
+ * Copyright (C) 2022 Tian Tan <tiantan@nju.edu.cn>
+ * Copyright (C) 2022 Yue Li <yueli@nju.edu.cn>
+ *
+ * This file is part of Tai-e.
+ *
+ * Tai-e is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Tai-e is distributed in the hope that it will be useful,but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Tai-e. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package pascal.taie.analysis.dataflow.solver;
+
+import fj.P;
+import pascal.taie.analysis.dataflow.analysis.DataflowAnalysis;
+import pascal.taie.analysis.dataflow.fact.DataflowResult;
+import pascal.taie.analysis.graph.cfg.CFG;
+
+import java.util.Set;
+
+class IterativeSolver<Node, Fact> extends Solver<Node, Fact> {
+
+    public IterativeSolver(DataflowAnalysis<Node, Fact> analysis) {
+        super(analysis);
+    }
+
+    @Override
+    protected void doSolveForward(CFG<Node> cfg, DataflowResult<Node, Fact> result) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    protected void doSolveBackward(CFG<Node> cfg, DataflowResult<Node, Fact> result) {
+        // TODO - finish me
+
+        boolean changed = true;
+
+        while (changed) {
+            changed = false;
+            for (Node node : cfg) {
+                if(cfg.isExit(node)) continue;
+
+                //更改 OUT
+                result.setOutFact(node, analysis.newInitialFact());     //去除原有的内容
+                for (Node succ : cfg.getSuccsOf(node)) {
+                    Fact succInFact = result.getInFact(succ);
+                    analysis.meetInto(succInFact, result.getOutFact(node));
+                }
+                //更改 IN
+                boolean change = analysis.transferNode(node, result.getInFact(node), result.getOutFact(node));
+                changed = change || changed;                            //只要一个改变即视为改变
+            }
+        }
+
+    }
+}
